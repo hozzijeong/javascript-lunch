e4086c1ff84fd805fa6a2ac868a0001a9c50a70f
hozzijeong <ghwls0905@naver.com>
2023-03-10 17:11:31 +0900

    chore: 이전 파일 제거

--- src/utils/util.js
+++
@@ -1,7 +0,0 @@
-export const arrayElementToObject = function arrayElementToValueTextObject(
-  array
-) {
-  return array.map((element) => {
-    return { value: element, text: element };
-  });
-};
