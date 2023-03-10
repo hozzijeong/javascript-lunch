89e9f8b27dfa6e98560c1cde7e5bc2197f83009e
hozzijeong <ghwls0905@naver.com>
2023-03-10 17:13:26 +0900

    build step2

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import '../templates/style.css';
-<<<<<<< HEAD
-
-import App from './App';
-
-const $app = document.getElementById('app');
-
-new App($app);
-=======
 import App from './App';
 
 const $app = document.getElementById('app');
 if ($app instanceof HTMLDivElement) {
   new App($app);
 }
->>>>>>> a210a190c8c8ba6ade2c3bf78d490af3006dc894
